0d58902d30daa3407fa2801784720b35bb7f1d01
RamejaAyush <ayushrwork@gmail.com>
2022-01-22 22:38:41 +0530

    logo animation added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
+import { useRef, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { TweenMax, Power3 } from "gsap";
+
 function App() {
+ let logoItem = useRef(null);
+ let text = useRef(null);
+
+ useEffect(() => {
+  TweenMax.to(logoItem, 2, {
+   y: -20,
+   scale: 1,
+   duration: Power3.easeInOut,
+  });
+
+  TweenMax.to(text, 0.8, {
+   y: -20,
+   opacity: 1,
+   duration: Power3.easeInOut,
+   stagger: 1,
+  });
+ }, []);
+
  return (
   <div className="App">
    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
+    <img
+     ref={(el) => {
+      logoItem = el;
+     }}
+     src={logo}
+     className="App-logo"
+     alt="logo"
+    />
+    <p
+     ref={(el) => {
+      text = el;
+     }}
+    >
      Edit <code>src/App.js</code> and save to reload.
     </p>
-    <a
+    {/* <a
      className="App-link"
      href="https://reactjs.org"
      target="_blank"
      rel="noopener noreferrer"
     >
      Learn React
-    </a>
+    </a> */}
    </header>
   </div>
  );
